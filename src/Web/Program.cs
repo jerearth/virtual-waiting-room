c72b36fc7951e3702636e58296af4b1fe1c55e14
viliam.poproci <viliam.poproci@gmail.com>
2023-11-26 18:09:03 +0100

    updated .NET 7.0 to .NET 8.0, completly replaced blazor project due to convention changes

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,14 +1,15 @@
 using Database;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
-using Web.Data;
+using System;
+using Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
 
 // Setup Database connection
 var connectionString = builder.Configuration.GetConnectionString("WaitBuddy");
@@ -32,19 +33,21 @@ app.UseForwardedHeaders(
     }
 );
 
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
+app.UseAntiforgery();
 
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
