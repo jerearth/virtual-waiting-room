c20a6d82ad2f255a1aeb8ecbc2a691bf8300aba4
viliam.poproci <viliam.poproci@gmail.com>
2023-12-02 09:14:46 +0100

    add migration and design context factory

---
+++ src/Database/AppDesignContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database
+{
+    public class AppDesignContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        private const string _dummyConnectionString = @"Server=localhost;Database=webapp_db;Uid=root;Pwd=root;";
+
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseMySql(_dummyConnectionString, serverVersion: ServerVersion.AutoDetect(_dummyConnectionString));
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
